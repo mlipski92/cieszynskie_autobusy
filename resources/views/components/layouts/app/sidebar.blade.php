f07c024aec33213d5a3aedbfff3ef1927516ad9f
Mateusz Lipski <80196434+mlipski92@users.noreply.github.com>
2025-06-16 22:49:59 +0200

    Add stop to nav

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -12,6 +12,7 @@
                 <flux:navlist.group :heading="__('Platform')" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
                     <flux:navlist.item icon="home" :href="route('trans.list')" :current="request()->routeIs('trans.list')" wire:navigate>{{ __('Lista przewoźników') }}</flux:navlist.item>
+                    <flux:navlist.item icon="home" :href="route('stop.list')" :current="request()->routeIs('stop.list')" wire:navigate>{{ __('Lista przystanków') }}</flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
 
