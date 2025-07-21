2f9affa8716a588ccbce024dad9bb9c2d28e96fe
Mateusz Lipski <80196434+mlipski92@users.noreply.github.com>
2025-07-21 18:11:37 +0200

    Fix issues

--- routes/web.php
+++ routes/web.php
@@ -16,120 +16,13 @@ use App\Models\LineStopRelation;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-// use Illuminate\Support\Facades\Route;
-// Route::get('/test', function () {
-//     $matchingLines = App\Models\LineStopRelation::whereIn('id_stop', [2, 4])
-//     ->get()
-//     ->groupBy('id_line')
-//     ->filter(function ($group) {
-//         $stop2 = $group->firstWhere('id_stop', 2);
-//         $stop4 = $group->firstWhere('id_stop', 4);
-//         return $stop2 && $stop4 && $stop2->order < $stop4->order;
-//     });
+    Route::get('/kup-bilet', [FrontController::class, 'buyTicket'])->name('bilet.kup');
+    Route::get('/success', [FrontController::class, 'successPage'])->name('success');
 
-
-//     $linesStops = LineStopRelation::where('id_line', $id)
-//     ->leftJoin('stops', 'line_stop_relations.id_stop', '=', 'stops.id')
-//     ->orderBy('line_stop_relations.order')
-//     ->get([
-//         'line_stop_relations.*',
-//         'stops.name as stop_name'
-//     ]);
-
-
-
-//     return [
-//     'lineStops' => $linesStops,
-//     'beginStop' => $matchingLines->first()[0],
-//     'endStop' => $matchingLines->first()[1]
-//     ];
-
-// });
-
-Route::get('/kup-bilet', [FrontController::class, 'buyTicket'])->name('bilet.kup');
-Route::get('/success', [FrontController::class, 'successPage'])->name('success');
-
-Route::post('/checkout', [FrontController::class, 'checkout']);
-Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-
-// Route::get('/test-trans', function () {
-//     // Dane OAuth
-//     $clientId = '01JZNXT3TS2DC0TBKJGPWJV587-01K06FQYM3GVQYN7JVWFXV9TYM';
-//     $clientSecret = '36702dd0b89bb1141bb4b7e599d4d55f5d25b7a1d9441da6811641d567c8f7b9';
-
-//     // Autoryzacja (Basic Auth do tokenu)
-//     $auth = base64_encode("$clientId:$clientSecret");
-
-//     $ch = curl_init();
-//     curl_setopt_array($ch, [
-//         CURLOPT_URL => 'https://api.tpay.com/oauth/auth',
-//         CURLOPT_RETURNTRANSFER => true,
-//         CURLOPT_POST => true,
-//         CURLOPT_HTTPHEADER => [
-//             'Authorization: Basic ' . $auth,
-//             'Content-Type: application/x-www-form-urlencoded',
-//         ],
-//         CURLOPT_POSTFIELDS => 'grant_type=client_credentials',
-//     ]);
-//     $response = curl_exec($ch);
-//     curl_close($ch);
-
-//     $data = json_decode($response, true);
-//     if (!isset($data['access_token'])) {
-//         return 'Błąd przy pobieraniu access_token: ' . print_r($data, true);
-//     }
-
-//     $accessToken = $data['access_token'];
-
-//     // Przygotowanie danych transakcji
-//     $transactionData = [
-//         'amount' => 0.1,
-//         'description' => 'Test transaction',
-//         'groupId' => 150, // ← Przelewy online
-//         'payer' => [
-//             'email' => 'jan.nowak@example.com',
-//             'name' => 'Jan Nowak'
-//         ],
-//         'callbacks' => [
-//             'notification' => [
-//                 'url' => 'https://zenitx.pl',
-//             ],
-//             'payerUrls' => [
-//                 'success' => 'https://zenitx.pl',
-//                 'error' => 'https://zenitx.pl',
-//             ],
-//         ],
-//     ];
-
-//     // Tworzenie transakcji
-//     $ch = curl_init();
-//     curl_setopt_array($ch, [
-//         CURLOPT_URL => 'https://api.tpay.com/transactions',
-//         CURLOPT_RETURNTRANSFER => true,
-//         CURLOPT_POST => true,
-//         CURLOPT_HTTPHEADER => [
-//             'Authorization: Bearer ' . $accessToken,
-//             'Content-Type: application/json',
-//         ],
-//         CURLOPT_POSTFIELDS => json_encode($transactionData),
-//     ]);
-
-//     $response = curl_exec($ch);
-//     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-//     curl_close($ch);
-
-//     $result = json_decode($response, true);
-// // dd($httpCode);
-//     if ($httpCode !== 200 || !isset($result['transactionPaymentUrl'])) {
-//         return "Błąd podczas tworzenia transakcji:<br><pre>" . print_r($result, true) . '</pre>';
-//     }
-
-//     // Przekierowanie użytkownika do strony płatności
-//    return redirect()->away($result['transactionPaymentUrl']);
-// });
+    Route::post('/checkout', [FrontController::class, 'checkout']);
+    Route::get('/', function () {
+        return view('home');
+    })->name('home');
 
 
     Route::view('dashboard', 'dashboard')
